17b3bf4445cf151d39072c7fd4f703ec55b3b325
GabeG-141 <gome0121@student.acitech.org>
2025-02-28 09:14:36 -0500

    Added a click counter
    I need to figure out how to link that variable to events

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const button1 = document.getElementById("button1");
 
-button1.addEventListener("click", yell_at_user);
+button1.addEventListener("click", first_interaction);
 
-function yell_at_user() {
-    alert("STOP PRESSING THE BUTTON!")
-
+function first_interaction()  {
+   alert("hey!")
 }
+
+var count = 0;
+var button = document.getElementById("button1");
+var countDisplay = document.getElementById("count");
+button.addEventListener("click", function() {
+   count++;
+   countDisplay.innerHTML ="Times clicked: " + count;
+});
